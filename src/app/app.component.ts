cc99e504f4ba34d8d566bef685e1b2a6d6687efc
tavershimafx <tatarprofessional@gmail.com>
2024-09-23 00:34:06 +0100

    mobile responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import isInbound from './utils/element';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'music-app-web';
+  
+  @ViewChild("side_bar", { static: true }) sideBar!: ElementRef
+  constructor(){
+    this.clickOut = this.clickOut.bind(this)
+  }
+
+  clickOut (e:MouseEvent){
+    let el = this.sideBar.nativeElement.getBoundingClientRect()
+    let b = isInbound(el.top, el.left, el.bottom, el.right, e.clientX, e.clientY)
+    if (!b){
+      this.sideBar.nativeElement.classList.remove("slide-in-mob")
+      this.sideBar.nativeElement.classList.add("slide-out-mob")
+      document.removeEventListener("click", this.clickOut)
+    }
+  }
+
+  toggleSideBar(){
+    if(this.sideBar.nativeElement.classList.contains("slide-out-mob")){
+      this.sideBar.nativeElement.classList.remove("slide-out-mob")
+      this.sideBar.nativeElement.classList.add("slide-in-mob")
+      setTimeout(() => {
+        document.addEventListener("click", this.clickOut)
+      }, 200);
+    }else{
+      this.sideBar.nativeElement.classList.remove("slide-in-mob")
+      this.sideBar.nativeElement.classList.add("slide-out-mob")
+    }
+  }
 }
